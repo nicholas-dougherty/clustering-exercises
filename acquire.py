40d1ab8be227e80a3b8277aa03c1ed326a849ac9
Nicholas Dougherty <nicholas.j.dougherty@gmail.com>
2022-03-31 17:15:29 -0500

    It's cold.

---
+++ acquire.py
@@ -0,0 +1,109 @@
+from env import host, username, password, get_db_url
+import os
+import pandas as pd 
+import numpy as np
+
+#_____________________________________________________________________________________________________________________________________________
+#_____________________________________________________________________________________________________________________________________________
+#_____________________________________________________________________________________________________________________________________________
+
+def get_db_url(database, username=username, host=host, password=password):
+    return f"mysql+pymysql://{username}:{password}@{host}/{database}"
+#_____________________________________________________________________________________________________________________________________________
+#_____________________________________________________________________________________________________________________________________________
+
+def acquire_zillow_data(use_cache=True):
+    '''
+    This function returns a snippet of zillow's database as a Pandas DataFrame. 
+    When this SQL data is cached and extant in the os directory path, return the data as read into a df. 
+    If csv is unavailable, aquisition proceeds regardless,
+    reading the queried database elements into a dataframe, creating a cached csv file
+    and lastly returning the dataframe for some sweet data science perusal.
+    '''
+
+    # If the cached parameter is True, read the csv file on disk in the same folder as this file 
+    if os.path.exists('zillow.csv') and use_cache:
+        print('Using cached CSV')
+        return pd.read_csv('zillow.csv')
+
+    # When there's no cached csv, read the following query from Codeup's SQL database.
+    print('CSV not detected.')
+    print('Acquiring data from SQL database instead.')
+    df = pd.read_sql(
+        '''
+        SELECT *
+            FROM properties_2017
+                JOIN (SELECT parcelid, logerror, max(transactiondate) as transactiondate 
+            FROM predictions_2017 GROUP BY parcelid, logerror) as pred_2017 using(parcelid) 
+                LEFT JOIN airconditioningtype USING(airconditioningtypeid)
+                LEFT JOIN architecturalstyletype USING(architecturalstyletypeid)
+                LEFT JOIN buildingclasstype USING(buildingclasstypeid)
+                LEFT JOIN heatingorsystemtype USING(heatingorsystemtypeid)
+                LEFT JOIN propertylandusetype USING(propertylandusetypeid)
+                LEFT JOIN storytype USING(storytypeid)
+                LEFT JOIN typeconstructiontype USING(typeconstructiontypeid)
+            WHERE properties_2017.latitude is not null
+            AND properties_2017.longitude is not null;             
+        '''
+                    , get_db_url('zillow'))
+
+    # create a csv of the dataframe for the sake of efficiency. 
+    df.to_csv('zillow.csv', index=False)
+    
+    return df
+
+
+#_____________________________________________________________________________________________________________________________________________
+#_____________________________________________________________________________________________________________________________________________
+def describe_data(df):
+    '''
+    This function takes in a pandas dataframe and prints out the shape, datatypes, number of missing values, 
+    columns and their data types, summary statistics of numeric columns in the dataframe, as well as the value counts for categorical variables.
+    '''
+    # Print out the "shape" of our dataframe - rows and columns
+    print(f'This dataframe has {df.shape[0]} rows and {df.shape[1]} columns.')
+    print('')
+    print('--------------------------------------')
+    print('--------------------------------------')
+    
+    # print the datatypes and column names with non-null counts
+    print(df.info())
+    print('')
+    print('--------------------------------------')
+    print('--------------------------------------')
+
+    # print the number of missing values per column and the total
+    print('Null Values: ')
+    missing_total = df.isnull().sum().sum()
+    missing_count = df.isnull().sum() # the count of missing values
+    value_count = df.isnull().count() # the count of all values
+    missing_percentage = round(missing_count / value_count * 100, 2) # percentage of missing values
+    missing_df = pd.DataFrame({'count': missing_count, 'percentage': missing_percentage}) # create df
+    print(missing_df)
+    print(f' \n Total Number of Missing Values: {missing_total} \n')
+    df_total = df[df.columns[:]].count().sum()
+    proportion_of_nulls = round((missing_total / df_total), 4)
+    print(f' Proportion of Nulls in Dataframe: {proportion_of_nulls}\n') 
+    print('--------------------------------------')
+    print('--------------------------------------')
+
+    # print out summary stats for our dataset
+    print('Here are the summary statistics of our dataset')
+    print(df.describe().applymap(lambda x: f"{x:0.3f}"))
+    print('')
+    print('--------------------------------------')
+    print('--------------------------------------')
+
+    print('Relative Frequencies: \n')
+    # Display top 5 values of each variable within reasonable limit
+    limit = 25
+    for col in df.columns:
+        if df[col].nunique() < limit:
+            print(f'Column: {col} \n {round(df[col].value_counts(normalize=True).nlargest(5), 3)} \n')
+        else: 
+            print(f'Column: {col} \n')
+            print(f'Range of Values: [{df[col].min()} - {df[col].max()}] \n')
+        print('------------------------------------------')
+        print('--------------------------------------')
+
+
