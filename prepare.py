ee58a7458621586038bc21f9b906499922cd58a2
Nicholas Dougherty <nicholas.j.dougherty@gmail.com>
2022-05-12 14:14:13 -0500

    considerable cleanups but retained initial content

--- prepare.py
+++ prepare.py
@@ -1,14 +1,23 @@
+#          MM"""""""`YMM"""""""`MMM""""""""`MM"""""""`YMMP"""""""MMM"""""""`MMM""""""""`M           # 
+#          MM  mmmmm  MM  mmmm,  MM  mmmmmmmMM  mmmmm  M' .mmmm  MMM  mmmm,  MM  mmmmmmmM           # 
+#          M'        .M'        .M`      MMMM'        .M         `M'        .M`      MMMM           # 
+#          MM  MMMMMMMMM  MMMb. "MM  MMMMMMMMM  MMMMMMMM  MMMMM  MMM  MMMb. "MM  MMMMMMMM           # 
+#          MM  MMMMMMMMM  MMMMM  MM  MMMMMMMMM  MMMMMMMM  MMMMM  MMM  MMMMM  MM  MMMMMMMM           # 
+#          MM  MMMMMMMMM  MMMMM  MM        .MM  MMMMMMMM  MMMMM  MMM  MMMMM  MM        .M           # 
+#          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM           #
+#--------------------------------------------------------------------------------------------------|#
+# IMPORTS
 from collections import Counter
 import pandas as pd
 import numpy as np
 from datetime import date
-
+#--------------------------------------------------------------------------------------------------|#
 def handle_missing_values(df, prop_required_column=0.5 , prop_required_row=0.5):
     '''
-    This function takes in a pandas dataframe, default proportion of required columns (set to 50%) and proprtion of required rows (set to 75%).
-    It drops any rows or columns that contain null values more than the threshold specified from the original dataframe and returns that dataframe.
-    
-    Prior to returning that data, it will print statistics and list counts/names of removed columns/row counts 
+    This function takes in a pandas dataframe, default proportion of required columns (set to 50%)
+    and proportion of required rows (set to 75%). It drops any rows or columns that contain null
+    values more than the threshold specified from the original dataframe and returns that dataframe.
+    Prior to returning that data, prints statistics and list counts/names of removed rows/cols 
     '''
     original_cols = df.columns.to_list()
     original_rows = df.shape[0]
@@ -20,12 +29,16 @@ def handle_missing_values(df, prop_required_column=0.5 , prop_required_row=0.5):
     remaining_rows = df.shape[0]
     dropped_col_count = len(original_cols) - len(remaining_cols)
     dropped_cols = list((Counter(original_cols) - Counter(remaining_cols)).elements())
-    print(f'The following {dropped_col_count} columns were dropped because they were missing more than {prop_required_column * 100}% of data: \n{dropped_cols}\n')
+    
+    print(f'The following {dropped_col_count} columns were dropped because they were missing more\
+    than {prop_required_column * 100}% of data: \n{dropped_cols}\n')
     dropped_rows = original_rows - remaining_rows
-    print(f'{dropped_rows} rows were dropped because they were missing more than {prop_required_row * 100}% of data')
+    
+    print(f'{dropped_rows} rows were dropped because they were missing more than
+          {prop_required_row * 100}% of data')
+          
     return df
-
-
+#--------------------------------------------------------------------------------------------------|#
 # combined in one function
 def data_prep(df, cols_to_remove=[], prop_required_column=0.5, prop_required_row=0.5):
     '''
@@ -35,20 +48,18 @@ def data_prep(df, cols_to_remove=[], prop_required_column=0.5, prop_required_row
     df = remove_columns(df, cols_to_remove)
     df = handle_missing_values(df, prop_required_column, prop_required_row)
     return df
-
+#--------------------------------------------------------------------------------------------------|#
 def remove_columns(df, cols_to_remove):
     '''
-    This function takes in a pandas dataframe and a list of columns to remove. It drops those columns from the original df and returns the df.
+    This function takes in a pandas dataframe and a list of columns to remove.
+    It drops those columns from the original df and returns the df.
     '''
     df = df.drop(columns=cols_to_remove)
     return df
-
+#--------------------------------------------------------------------------------------------------|#
 
 def remove_outliers(df, k, col_list):
-    ''' remove outliers from a list of columns in a dataframe 
-        and return that dataframe
-    '''
-    
+    ''' remove outliers from a list of columns in a dataframe and return that dataframe'''  
     for col in col_list:
         # get quartiles
         q1, q3 = df[f'{col}'].quantile([.25, .75])  
@@ -58,43 +69,34 @@ def remove_outliers(df, k, col_list):
         upper_bound = q3 + k * iqr 
         # get lower bound
         lower_bound = q1 - k * iqr   
-
-        # return dataframe without outliers
-        
-        df = df[(df[f'{col}'] > lower_bound) & (df[f'{col}'] < upper_bound)]
-        
+        # return dataframe without outliers        
+        df = df[(df[f'{col}'] > lower_bound) & (df[f'{col}'] < upper_bound)]        
     return df
-
+#--------------------------------------------------------------------------------------------------|#
 
 def prep_zillow(df):
-    
-    df = data_prep(df)
-    
+    '''
+    Multifaceted Data preparation broken down by codecomments
+    Returns clean dataframe
+    '''
+    df = data_prep(df)    
     df = df[(df.propertylandusedesc == 'Single Family Residential') |
       (df.propertylandusedesc == 'Mobile Home') |
       (df.propertylandusedesc == 'Manufactured, Modular, Prefabricated Homes') |
-      (df.propertylandusedesc == 'Cluster Home')]
-    
+      (df.propertylandusedesc == 'Cluster Home')]    
     # Remove properties that couldn't even plausibly be a studio. 
-    df= df[(df.bedroomcnt > 0) & (df.bathroomcnt > 0)]
-    
+    df= df[(df.bedroomcnt > 0) & (df.bathroomcnt > 0)]    
     # Remove properties where there is not a single bathroom.
-    df = df[df.bathroomcnt > 0]
-    
- # keep only properties with square footage greater than 70 (legal size of a bedroom)
-    df = df[df.calculatedfinishedsquarefeet > 70]
-    
+    df = df[df.bathroomcnt > 0]    
+    # keep only properties with square footage greater than 70 (legal size of a bedroom)
+    df = df[df.calculatedfinishedsquarefeet > 70]    
     # Minimum lot size of single family units.
-    df = df[df.lotsizesquarefeet >= 5000].copy()
-    
-    
-    
+    df = df[df.lotsizesquarefeet >= 5000].copy()    
     #df = df[~df['propertylandusetypeid'].isin([263, 265, 275])]
-    
     # Clear indicators of single unit family. Other codes non-existent or indicate commercial sites. 
-   # 0100 - Single Residence
-   # 0101 Single residence with pool
-   # 0104 - Single resident with therapy pool 
+    # 0100 - Single Residence
+    # 0101 Single residence with pool
+    # 0104 - Single resident with therapy pool 
     df = df[(df.propertycountylandusecode == '0100') |
             (df.propertycountylandusecode == '0101') |
             (df.propertycountylandusecode == '0104') |
@@ -102,19 +104,13 @@ def prep_zillow(df):
             (df.propertycountylandusecode == '1111') |
             (df.propertycountylandusecode == '1110') |
             (df.propertycountylandusecode == '1')
-           ]
-    
-    
-    # Remove 13 rows where unit count is 2. The NaN's can be safely assumed as 1 and were just mislabeled in other counties.  
+           ]   
+    # Remove 13 rows where unit count is 2. The NaN's can be safely
+    # assumed as 1 and were just mislabeled in other counties.  
     df = df[df['unitcnt'] != 2]
-    df['unitcnt'].fillna(1)
-    
-    
+    df['unitcnt'].fillna(1) 
     # Property where finished area is 152 but bed count is 5. 
-    df = df.drop(labels=75325, axis=0)
-    
-      
-            
+    df = df.drop(labels=75325, axis=0)          
     # Redudant columns or uninterpretable columns
     # Unit count was dropped because now its known that theyre all 1. 
     # Finished square feet is equal to calculated sq feet. 
@@ -126,61 +122,51 @@ def prep_zillow(df):
     # room count must be for a different category, as it is always 0.
     # regionidcounty reveals the same information as FIPS. 
     # heatingorsystemtypeid is redundant. Encoded descr. 
-    # Id does nothing, and parcelid is easier to represent. 
-
-    
+    # Id does nothing, and parcelid is easier to represent.
     df =df.drop(columns= ['finishedsquarefeet12', 'fullbathcnt', 'calculatedbathnbr',
                       'propertyzoningdesc', 'unitcnt', 'propertylandusedesc',
                       'assessmentyear', 'roomcnt', 'regionidcounty', 'propertylandusetypeid',
                       'heatingorsystemtypeid', 'id', 'heatingorsystemdesc', 'buildingqualitytypeid'],
-            axis=1)
-    
-    
+            axis=1)    
     # The last nulls can be dropped altogether. 
     df = df.dropna()
- 
-
     df['yearbuilt'] = df['yearbuilt'].astype(int)
     df.yearbuilt = df.yearbuilt.astype(object) 
     df['age'] = 2017-df['yearbuilt']
     df = df.drop(columns='yearbuilt')
     df['age'] = df['age'].astype('int')
-    print('Yearbuilt converted to age. \n')
-    
+    print('Yearbuilt converted to age. \n')   
     # Removing problematic outlier groups.  
     df = remove_outliers(df, 3, ['lotsizesquarefeet', 'structuretaxvaluedollarcnt', 'taxvaluedollarcnt',
-                                'landtaxvaluedollarcnt', 'taxamount', 'calculatedfinishedsquarefeet'])
-    
+                                'landtaxvaluedollarcnt', 'taxamount', 'calculatedfinishedsquarefeet']) 
     df = df.set_index('parcelid')
     
     return df
-
+#--------------------------------------------------------------------------------------------------|#
 
 def describe_data(df):
     '''
-    This function takes in a pandas dataframe and prints out the shape, datatypes, number of missing values, 
-    columns and their data types, summary statistics of numeric columns in the dataframe, as well as the value counts for categorical variables.
+    This function takes in a pandas dataframe and prints out the shape,
+    datatypes, number of missing values, columns and their data types,
+    summary statistics of numeric columns in the dataframe, as well as
+    the value counts for categorical variables.
     '''
     # Print out the "shape" of our dataframe - rows and columns
     print(f'This dataframe has {df.shape[0]} rows and {df.shape[1]} columns.')
     print('')
     print('--------------------------------------')
-    print('--------------------------------------')
-    
+    print('--------------------------------------')    
     # print the datatypes and column names with non-null counts
     print(df.info())
     print('')
     print('--------------------------------------')
-    print('--------------------------------------')
-    
-    
+    print('--------------------------------------')    
     # print out summary stats for our dataset
     print('Here are the summary statistics of our dataset')
     print(df.describe().applymap(lambda x: f"{x:0.3f}"))
     print('')
     print('--------------------------------------')
     print('--------------------------------------')
-
     # print the number of missing values per column and the total
     print('Null Values by Column: ')
     missing_total = df.isnull().sum().sum()
@@ -188,21 +174,17 @@ def describe_data(df):
     value_count = df.isnull().count() # the count of all values
     missing_percentage = round(missing_count / value_count * 100, 2) # percentage of missing values
     missing_df = pd.DataFrame({'count': missing_count, 'percentage': missing_percentage})\
-    .sort_values(by='percentage', ascending=False)
-    
+    .sort_values(by='percentage', ascending=False)    
     print(missing_df.head(50))
     print(f' \n Total Number of Missing Values: {missing_total} \n')
     df_total = df[df.columns[:]].count().sum()
     proportion_of_nulls = round((missing_total / df_total), 4)
     print(f' Proportion of Nulls in Dataframe: {proportion_of_nulls}\n') 
     print('--------------------------------------')
-    print('--------------------------------------')
-    
+    print('--------------------------------------')    
     print('Row-by-Row Nulls')
     print(nulls_by_row(df))
     print('----------------------')
-
-
     print('Relative Frequencies: \n')
     # Display top 5 values of each variable within reasonable limit
     limit = 25
@@ -214,12 +196,13 @@ def describe_data(df):
             print(f'Range of Values: [{df[col].min()} - {df[col].max()}] \n')
         print('------------------------------------------')
         print('--------------------------------------')
-        
-        
+#--------------------------------------------------------------------------------------------------|#        
 def nulls_by_col(df):
     '''
-    This function  takes in a dataframe of observations and attributes(or columns) and returns a dataframe where each row is an atttribute name, the first column is the 
-    number of rows with missing values for that attribute, and the second column is percent of total rows that have missing values for that attribute.
+    This function  takes in a dataframe of observations and attributes(or columns)
+    and returns a dataframe where each row is an atttribute name, the first column is the 
+    number of rows with missing values for that attribute, and the second column is percent
+    of total rows that have missing values for that attribute.
     '''
     num_missing = df.isnull().sum()
     rows = df.shape[0]
@@ -228,10 +211,11 @@ def nulls_by_col(df):
                                  'percent_rows_missing': prcnt_miss})\
     .sort_values(by='percent_rows_missing', ascending=False)
     return cols_missing.applymap(lambda x: f"{x:0.1f}")
-
+#--------------------------------------------------------------------------------------------------|#
 def nulls_by_row(df):
     '''
-    This function takes in a dataframe and returns a dataframe with 3 columns: the number of columns missing, percent of columns missing, 
+    This function takes in a dataframe and returns a dataframe with 3 columns:
+    the number of columns missing, percent of columns missing, 
     and number of rows with n columns missing.
     '''
     num_missing = df.isnull().sum(axis = 1)
@@ -243,6 +227,5 @@ def nulls_by_row(df):
     .rename(index=str, columns={'index': 'num_rows'}).reset_index().set_index('num_cols_missing')\
     .sort_values(by='percent_cols_missing', ascending=False)
     return rows_missing
-    
-    
-    
+#--------------------------------------------------------------------------------------------------|#
+#--------------------------------------------------------------------------------------------------|#   
